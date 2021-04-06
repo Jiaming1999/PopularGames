bb95a23a8355ada8b735ec03dc0588096f5d3e3b
jz32 <jz32@illinois.edu>
2021-04-05 21:54:49 -0500

    Revert "Merge branch 'project-1.0' into 'master'"
    This reverts merge request !4

--- src/config.py
+++
@@ -1,8 +0,0 @@
-"""
-Global Var for src
-"""
-SCROLL_PAUSE_TIME = 0.5
-SCROLL_TIMES = 10
-GAMES_URL = "https://www.ign.com/reviews/games"
-IGN_URL = "https://www.ign.com"
-TOP_100_GAMES = "https://www.ign.com/lists/top-100-games/"
