491ce55967980f03b6100981993a4d5fb208a455
Jiaming <qq1104382148@gmail.com>
2021-04-12 20:05:22 -0500

    commenting

--- test/api_test.py
+++ test/api_test.py
@@ -8,34 +8,55 @@ class MyTestCase(unittest.TestCase):
         self.app = app.test_client()
 
     def test_get_popular_limit_50(self):
+        """
+        test if successfully get 50 popular games
+        """
         response = self.app.get('http://127.0.0.1:5000/popular?limit=50')
         self.assertEqual(response.status_code, 201)
 
     def test_get_top_limit_10(self):
+        """
+        test if successfully get 10 top100 games
+        """
         response = self.app.get('http://127.0.0.1:5000/top100?limit=10')
         self.assertEqual(response.status_code, 201)
 
     def test_put_popular_score(self):
+        """
+        test if successfully update score to 8 for popular game with given id
+        """
         url = 'http://127.0.0.1:5000/popular?id=6073cc31c53d959a436ba25e'
         response = self.app.put(url, data=json.dumps({'score': 8}), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_put_popular_review(self):
+        """
+        test if successfully update review for popular game with given id
+        """
         url = 'http://127.0.0.1:5000/popular?id=6073cc31c53d959a436ba25e'
         response = self.app.put(url, data=json.dumps({'review': 'New Review'}), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_put_top_score(self):
+        """
+        test if successfully update score to 7.9 for top game with given id
+        """
         url = 'http://127.0.0.1:5000/top100?id=6073cb9fc53d959a436ba1fa'
         response = self.app.put(url, data=json.dumps({'score': 7.9}), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_put_top_review(self):
+        """
+        test if successfully update review for top game with given id
+        """
         url = 'http://127.0.0.1:5000/top100?id=6073cb9fc53d959a436ba1fa'
         response = self.app.put(url, data=json.dumps({'review': 'New Review'}), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_post_popular(self):
+        """
+        test to insert a new popular game
+        """
         test_data = {
             "developers": "Jiaming",
             "editor": "Jiaming Zhang",
@@ -59,10 +80,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_delete_popular(self):
+        """
+        test to delete a popular game by title
+        """
         response = self.app.delete("http://127.0.0.1:5000/popular?title=TestGame")
         self.assertEqual(response.status_code, 201)
 
     def test_post_top(self):
+        """
+        test to insert a top100 game
+        """
         test_data = {
             "developers": "Jiaming",
             "editor": "Jiaming Zhang",
@@ -86,14 +113,23 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_delete_top(self):
+        """
+        test to delete a top100 game by title
+        """
         response = self.app.delete("http://127.0.0.1:5000/top100?title=TestGame")
         self.assertEqual(response.status_code, 201)
 
     def test_filter_popular_genre(self):
+        """
+        test to get most common genre in popular games
+        """
         response = self.app.get('http://127.0.0.1:5000/filter/popular?type=mostgenre')
         self.assertEqual(response.status_code, 201)
 
     def test_filter_top_platform(self):
+        """
+        test to get least common platform in top games
+        """
         response = self.app.get('http://127.0.0.1:5000/filter/popular?type=leastplatform')
         self.assertEqual(response.status_code, 201)
 
